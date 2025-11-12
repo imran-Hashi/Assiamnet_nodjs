c21b33039dffe528d27191610ce79b565201300a
Imran Hashi <imranhashi@gmail.com>
2025-11-12 11:25:20 +0300

    first commit

--- app.js
+++ app.js
@@ -7,26 +7,26 @@ async function run() {
     await client.connect();
     console.log('Connected to MongoDB');
 
-    const db = client.db('unversity');  // Database sax ah
-    const students = db.collection('student');  // Collection sax ah
+    const db = client.db('unversity');  
+    const students = db.collection('student');
 
-    // 1. Insert many documents
+    
     const insertManyResult = await students.insertMany([
-      { name: 'najmo', age: 20, department: 'Computer Science' },
-      { name: 'casha', age: 21, department: 'Information Technology' },
-      { name: 'ali', age: 23, department: 'Software Engineering' }
+      { name: 'bakar', age: 20, department: 'Computer Science' },
+      { name: 'casha', age: 21, department: 'Economic' },
+      { name: 'sacdia', age: 23, department: 'public health' }
     ]);
     console.log('Inserted multiple students:', insertManyResult.insertedIds);
 
-    // 2. Update one document
+    
     const updateResult = await students.updateOne(
-      { name: 'ahmed' },  // filter - cidda la update gareynayo
-      { $set: { age: 21 } }  // waxa la bedelayo
+      { name: 'faarax' },  
+      { $set: { age: 21 } }  
     );
     console.log('Updated documents count:', updateResult.modifiedCount);
 
-    // 3. Delete one document
-    const deleteResult = await students.deleteOne({ name: 'ali' });
+   
+    const deleteResult = await students.deleteOne({ name: 'faaduma' });
     console.log('Deleted documents count:', deleteResult.deletedCount);
 
   } catch (err) {
